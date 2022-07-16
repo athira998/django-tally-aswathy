5ead7e4f07bfa0ac10a7596688d5208743dd007d
athira998 <athiraknair998@gmail.com>
2022-07-16 21:35:17 +0530

    third commit

--- app1/urls.py
+++ app1/urls.py
@@ -20,9 +20,22 @@ urlpatterns = [
     path('load_stock_group',views.load_stock_group,name='load_stock_group'),
     path('load_stock_catagory',views.load_stock_catagory,name='load_stock_catagory'),
     path('load_unit_creation',views.load_unit_creation,name='load_unit_creation'),
-    path('load_stock_item',views.load_stock_item,name='load_stock_item'),
+    path('load_unit_compound',views.load_unit_compound,name='load_unit_compound'),
+    
+    path('load_stock_item_creation',views.load_stock_item_creation,name='load_stock_item_creation'),
     path('load_company_price',views.load_company_price,name='load_company_price'),
     path('load_pan_cin',views.load_pan_cin,name='load_pan_cin'),
+    path('godown_creation',views.godown_creation,name='godown_creation'),
+    path('load_tds',views.load_tds,name='load_tds'),
+    path('load_person_res',views.load_person_res,name='load_person_res'),
+    path('load_gst_details',views.load_gst_details,name='load_gst_details'),
+    path('load_gst',views.load_gst,name='load_gst'),
+    path('load_lut_bond',views.load_lut_bond,name='load_lut_bond'),
+    path('load_gst_details_c',views.load_gst_details_c,name='load_gst_details_c'),
+    path('load_rev',views.load_rev,name='load_rev'),
+
+
+
 
 
 
--- app1/views.py
+++ app1/views.py
@@ -43,8 +43,12 @@ def load_stock_catagory(request):
 def load_unit_creation(request):
     return render(request,'unit_creation.html')
 
-def load_stock_item(request):
-    return render(request,'stock_item.html')
+def load_unit_compound(request):
+    return render(request,'unit_compound.html')
+
+
+def load_stock_item_creation(request):
+    return render(request,'stock_item_creation.html')
 
 def load_company_price(request):
     return render(request,'company_price.html')
@@ -52,3 +56,27 @@ def load_company_price(request):
 
 def load_pan_cin(request):
     return render(request,'pan_cin.html')
+
+def godown_creation(request):
+    return render(request,'godown.html')
+
+def load_tds(request):
+    return render(request,'tds_details.html')
+
+def load_person_res(request):
+    return render(request,'person_res.html')
+
+def load_gst_details(request):
+    return render(request,'gst_details.html')
+
+def load_gst(request):
+    return render(request,'gst.html')
+
+def load_lut_bond(request):
+    return render(request,'lut_bond.html')
+
+def load_gst_details_c(request):
+    return render(request,'gst_details_c.html')
+
+def load_rev(request):
+    return render(request,'revised.html')
